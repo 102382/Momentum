bb8775b298c1068f1f828bd81d08112e40a46697
Mohammed <102382@glr.nl>
2026-06-10 12:24:17 +0200

    Vercel fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import UserAuthForms from "./componentes/userAuthForms/UserAuthForms.jsx";
-import GegevensFormulier from "./componentes/gegevensformulier/GegevensFormulier.jsx";
-
 
 export default function Home() {
   return (
