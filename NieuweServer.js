18c7cecdc939bb10126f593fbfd334af0b28b37e
Mohammed <102382@glr.nl>
2026-06-18 14:17:15 +0200

    Bug fix
    Het is steeds niet gelukt. Ik kan steeds geen account aanmaken online

--- NieuweServer.js
+++ NieuweServer.js
@@ -137,8 +137,9 @@ const GebruikersOpdrachten = mongoose.model(
 // =========================
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
